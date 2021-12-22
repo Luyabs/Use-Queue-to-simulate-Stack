45487b984e64952765d825cb658bb50ca987805a
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2021-12-22 21:42:36 +0800

    完善基本功能
    完善基本功能

--- QtoS/QStack.h
+++ QtoS/QStack.h
@@ -8,7 +8,8 @@ public:
 	QStack();
 	virtual ~QStack();
 	int GetLength() const;							// 求栈的长度			 
-	bool IsEmpty(int mode = 1) const;					// 判断栈中队列是否为空 //mode = 1 两条队列是否全空(栈为空)?	mode = 2 至少有一条队列为空?	mode = 3 有且仅有一条空队列? 
+	bool IsEmpty(int mode = 1) const;				// 判断栈中队列是否为空 //mode = 1 两条队列是否全空(栈为空)?	mode = 2 至少有一条队列为空?	mode = 3 有且仅有一条空队列? 
+	bool IsFull(int mode = 1) const;				// 判断栈中队列是否已满 
 	Status Clear();									// 将栈清空
 	void Traverse(void (*Visit)(const ElemType&)) const;	// 遍历栈
 	Status Push(const ElemType e);				    // 入栈
@@ -18,23 +19,31 @@ public:
 	QStack<ElemType>& operator =(const QStack<ElemType>& s); // 赋值语句重载
 
 private:
-	SeqQueue<ElemType> *q;
+	SeqQueue<ElemType>* q;
 	int cur;	//当前队列 当一条表为空时 cur应指向有数据的表
 	int maxsize;
 };
 
 template<class ElemType>
-QStack<ElemType>::QStack() :maxsize(2 * DEFAULT_SIZE)
+QStack<ElemType>::QStack() :maxsize(2 * DEFAULT_SIZE), cur(0)
 {
 	q = new SeqQueue<ElemType>[2];
-	cur = 0;
 }
-
 
 template<class ElemType>
 QStack<ElemType>::~QStack()
 {
-	delete []q;
+	delete[]q;
+}
+
+template<class ElemType>
+int QStack<ElemType>::GetLength() const
+{
+	int count = 0;
+	for (int i = 0; i < 2; i++)
+		for (int j = q[i].front; j != q[i].rear; j = (j + 1) % q[i].maxSize)
+			count++;
+	return count;
 }
 
 template<class ElemType>
@@ -75,11 +84,11 @@ template<class ElemType>
 void QStack<ElemType>::Traverse(void (*Visit)(const ElemType&)) const
 {
 	int temp = cur;
-	for (int i = q[cur].front; i != q[cur].rear; i = (i + 1) % q[cur].maxSize)
-		(*Visit)(q[cur].elems[i]);
-	temp = 1 - cur;
-	for (int i = q[cur].front; i != q[cur].rear; i = (i + 1) % q[cur].maxSize)
-		(*Visit)(q[cur].elems[i]);
+	for (int i = q[temp].front; i != q[temp].rear; i = (i + 1) % q[temp].maxSize)
+		(*Visit)(q[temp].elems[i]);
+	temp = 1 - temp;
+	for (int i = q[temp].front; i != q[temp].rear; i = (i + 1) % q[temp].maxSize)
+		(*Visit)(q[temp].elems[i]);
 }
 
 template<class ElemType>
@@ -88,7 +97,7 @@ Status QStack<ElemType>::Push(const ElemType e)
 	int L = q[cur].GetLength();
 	if (L <= q[cur].maxSize)
 	{
-		q[1-cur].EnQueue(e);
+		q[1 - cur].EnQueue(e);
 		ElemType t;
 		for (int i = 0; i < L; i++)
 		{
@@ -105,6 +114,17 @@ Status QStack<ElemType>::Push(const ElemType e)
 }
 
 template<class ElemType>
+Status QStack<ElemType>::Top(ElemType& e) const
+{
+	if (IsEmpty(1))	 // 栈空
+		return UNDER_FLOW;
+	else { // 栈非空,操作成功
+		q[cur].GetHead(e);	// 用e返回栈顶元素
+		return SUCCESS;
+	}
+}
+
+template<class ElemType>
 Status QStack<ElemType>::Pop(ElemType& e)		//弹出"栈"顶元素
 {
 	if (IsEmpty(1))
@@ -116,5 +136,30 @@ Status QStack<ElemType>::Pop(ElemType& e)		//弹出"栈"顶元素
 	}
 }
 
+template<class ElemType>
+QStack<ElemType>::QStack(const QStack<ElemType>& s)
+{
+	maxsize = s.maxsize;
+	cur = s.cur;
+	q = new SeqQueue<ElemType>[2];		
+	for (int i = 0; i < 2; i++)
+		q[i] = s.q[i];
+}
+
+template<class ElemType>
+QStack<ElemType>& QStack<ElemType>::operator =(const QStack<ElemType>& s)
+{
+	if (&s != this) 
+	{
+		maxsize = s.maxsize;
+		cur = s.cur;
+		if (IsEmpty(1) != 0)
+			Clear();
+		q = new SeqQueue<ElemType>[2];
+		for (int i = 0; i < 2; i++)
+			q[i] = s.q[i];
+	}
+	return *this;
+}
 
 
