9515cb4856131b0805cc500e99c96048372f2950
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2021-12-23 14:56:19 +0800

    Update Test.cpp

--- QtoS/Test.cpp
+++ QtoS/Test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "QStack.h"
 
-double gettime(int restart=0)					// 参数带默认值，非零表示重新计时
+double gettime(int restart = 0)					// 参数带默认值，非零表示重新计时
 {											// 否则累计计时
 	const double c = 1.0 / CLOCKS_PER_SEC;
 	static double t = double(clock());				// 静态局部变量。第一次调用时，确定计时起点
@@ -13,14 +13,34 @@ int main()
 {
 	double e = 0;
 	QStack<double> s;
+	/*
+	s.Push_Switch(1);
+	s.Push_Switch(2);
+	s.Push_Switch(3);
+	s.Push_Switch(4);
+	s.Push_Switch(5);
+	s.Push_Switch(6);
+	s.Push_Switch(7);
+	s.Push_Switch(8);
+	s.Traverse(Write<double>);
+	cout << endl;
+	*/
+	//s.Push_Switch(666);	
+	//s.Push_Switch(999);
+
+	s.Push(0,11);
+	s.Traverse(Write<double>);
+	cout << endl;
+
+
 	//cout << s.IsEmpty(1) << endl;
 	//cout << s.IsEmpty(2) << endl;
 	//cout << s.IsEmpty(3) << endl;
 	cout << "IsEmpty3?  " << s.IsEmpty(3) << endl;
-	s.Push(0.5);
+	//s.Push(0.5);
 	s.Clear();
-	s.Push(1.2);
-	s.Push(3.4);
+	//s.Push(1.2);
+	//s.Push(3.4);
 
 	QStack<double> s_copy(s);
 	QStack<double> s_equal;
@@ -40,55 +60,55 @@ int main()
 	s.Traverse((Write<double>));	cout << endl;
 	cout << "IsEmpty3?  " << s.IsEmpty(3) << endl;
 }
-	/*
-	char c = '#';
-    QStack<int> sa;
-    int x;
-    while (c != '0')	{
-        cout << endl << "1. 生成栈.";
-        cout << endl << "2. 显示栈.";
-        cout << endl << "3. 入栈.";
-        cout << endl << "4. 出栈.";
-        cout << endl << "5. 取栈顶.";
-		cout << endl << "0. 退出";
-		cout << endl << "选择功能(0~5):";
-		cin >> c;
-		switch (c) 		{
-			case '1':
-			    sa.Clear();
-				cout << "输入e(e =0时退出)" << endl;
-    		    cin >> x;
-  				while (x != 0){
-					sa.Push(x);
-					cin >> x;
-                }
-				break;
-	       case '2':
-				cout << endl;
-			    sa.Traverse(Write<int>);
-				break;
-		   case '3':
-			    cout << endl << "输入元素值:";
-			    cin >> x;
-			    sa.Push(x);
-			    break;
-           case '4':
-			    if (sa.Pop(x) == SUCCESS)
-			           cout << endl << "栈顶元素值为：" << x << "." << endl;
-                else
-			           cout << endl << "栈为空." << endl;
-                break;
-           case '5':
-			    if (sa.Top(x) == SUCCESS)
-			           cout << endl << "栈顶元素值为：" << x << "." << endl;
-                else
-			           cout << endl << "栈为空." << endl;
-			    break;
-		}
+/*
+char c = '#';
+QStack<int> sa;
+int x;
+while (c != '0')	{
+	cout << endl << "1. 生成栈.";
+	cout << endl << "2. 显示栈.";
+	cout << endl << "3. 入栈.";
+	cout << endl << "4. 出栈.";
+	cout << endl << "5. 取栈顶.";
+	cout << endl << "0. 退出";
+	cout << endl << "选择功能(0~5):";
+	cin >> c;
+	switch (c) 		{
+		case '1':
+			sa.Clear();
+			cout << "输入e(e =0时退出)" << endl;
+			cin >> x;
+			while (x != 0){
+				sa.Push(x);
+				cin >> x;
+			}
+			break;
+	   case '2':
+			cout << endl;
+			sa.Traverse(Write<int>);
+			break;
+	   case '3':
+			cout << endl << "输入元素值:";
+			cin >> x;
+			sa.Push(x);
+			break;
+	   case '4':
+			if (sa.Pop(x) == SUCCESS)
+				   cout << endl << "栈顶元素值为：" << x << "." << endl;
+			else
+				   cout << endl << "栈为空." << endl;
+			break;
+	   case '5':
+			if (sa.Top(x) == SUCCESS)
+				   cout << endl << "栈顶元素值为：" << x << "." << endl;
+			else
+				   cout << endl << "栈为空." << endl;
+			break;
 	}
-
-	system("PAUSE");  
-	return 0;  */
 }
 
+system("PAUSE");
+return 0;  
+}
+*/
 
