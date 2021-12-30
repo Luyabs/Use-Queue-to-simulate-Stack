ca5f8f82c437e5bf55c7d45004700127f4b349b6
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2021-12-30 17:08:49 +0800

    gettime+

--- QtoS/QStack.h
+++ QtoS/QStack.h
@@ -5,7 +5,8 @@
 #include <fstream>
 #include <cmath>
 
-
+double gettime(int restart = 0);		
+	
 template<class ElemType> class QStack
 {
 public:
@@ -628,3 +629,12 @@ QStack<ElemType>& QStack<ElemType>::operator =(const QStack<ElemType>& s)
 	}
 	return *this;
 }
+
+double gettime(int restart)					// 参数带默认值，非零表示重新计时
+{											// 否则累计计时
+	const double c = 1.0 / CLOCKS_PER_SEC;
+	static double t = double(clock());				// 静态局部变量。第一次调用时，确定计时起点
+	if (restart) t = double(clock());				// 根据实参决定是否重新确定计时起点
+	return c * (double(clock()) - t);					// 从上一计时点到现在所经历的时间
+}
+
